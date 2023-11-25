3cf639b71b02f4ede3766532b21574fb7ab3d23b
Muhamad Sajid Bin Yahaya <98081971+tehtehking@users.noreply.github.com>
2023-11-25 08:03:53 +0800

    - Add extension library - Add timer in console app - Add new app settings config in console App

--- YamurConsoleApp/Program.cs
+++ YamurConsoleApp/Program.cs
@@ -1,13 +1,46 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using BusinessLayer;
+using Microsoft.Extensions.Configuration;
+using Newtonsoft.Json;
+using System.Collections.Generic;
 
-Console.WriteLine("Hello, World!");
+// Build a config object, using env vars and JSON providers.
+IConfigurationRoot config = new ConfigurationBuilder()
+    .AddJsonFile("appsettings.json")
+    .AddEnvironmentVariables()
+    .Build();
+
+// Get values from the config given their key and their target type.
+var settings = config.GetRequiredSection("Settings").Get<Dictionary<string, string?>>();
+
+// TODO - Get list of commands
+Dictionary<double, int> commands = await GetCommands();
+
+// Set the Time Interval order by the time duration.
+// Send Message
+foreach(var command in commands) 
+{
+
+
+}
 
 // Set the time interval (in milliseconds)
 int interval = 60000; // 60,000 milliseconds = 1 minute
 
 // Create a timer that calls the specified method every 'interval' milliseconds
-Timer timer = new Timer(async state => await CommandManagement.GetAutoCommandList(), null, 0, interval);
+Timer timer = new Timer(state => WriteSomething(), null, 0, interval);
 
-Console.WriteLine("Press Enter to stop the application.");
-Console.ReadLine();
+//Console.WriteLine("Press Enter to stop the application.");
+//Console.ReadLine();
+
+void WriteSomething()
+{
+    Console.WriteLine("1 minute has passed");
+}
+
+async Task<Dictionary<double, int>> GetCommands()
+{
+    return await CommandManagement.GetIntervalCommandList();
+
+}
+
